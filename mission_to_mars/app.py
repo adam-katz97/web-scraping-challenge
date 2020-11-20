d6dd0da7ece5fb36b0232ff76e1e801a5186f5a2
Adam Katz <adamkatz@Adams-MacBook-Pro-7.local>
2020-11-20 10:47:55 -0600

    properly formats table in jupyter notebook and edits python code a bit but the website still does not function properly.

--- mission_to_mars/app.py
+++ mission_to_mars/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
+import os
+import pandas as pd
 app = Flask(__name__)
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
--- mission_to_mars/scrape_mars.py
+++ mission_to_mars/scrape_mars.py
@@ -1,7 +1,10 @@
 from splinter import Browser
-from bs4 import BeautifulSoup
-import pandas
+from bs4 import BeautifulSoup as bs
+import pandas as pd
 import datetime
+import os
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 def init_browser():
     executable_path = {'executable_path': '/Users/adamkatz/.wdm/drivers/chromedriver/mac64/87.0.4280.20/chromedriver'}
