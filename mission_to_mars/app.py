e498009ba5969735e997d35f16548ed530369192
Adam Katz <adamkatz@Adams-MacBook-Pro-7.local>
2020-11-20 10:19:36 -0600

    adds jumbotron and scrape button to html.

--- mission_to_mars/app.py
+++ mission_to_mars/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_mars.py 
+import scrape_mars
 app = Flask(__name__)
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
