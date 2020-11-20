b2612916cda90b909ae14c2bf2430d5f05367abb
Adam Katz <adamkatz@Adams-MacBook-Pro-7.local>
2020-11-20 10:56:57 -0600

    properly copies the code from the jupyter notebook to the mars_scrapper.

--- mission_to_mars/app.py
+++ mission_to_mars/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 import os
-import pandas as pd
+import pandas as pd    
 app = Flask(__name__)
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
--- mission_to_mars/scrape_mars.py
+++ mission_to_mars/scrape_mars.py
@@ -2,6 +2,7 @@ from splinter import Browser
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 import datetime
+import requests
 import os
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -11,7 +12,28 @@ def init_browser():
     return Browser('chrome', **executable_path, headless=False)
 def scrape():
     browser = init_browser()
+    url = 'https://mars.nasa.gov/news/'
+    browser.visit(url)
+    html = browser.html 
+    soup = bs(html, 'lxml')
+    news_title=soup.find_all('div', class_='bottom_gradient')[0].text
+    news_p=soup.find_all('div', class_='article_teaser_body')[0].text
+    new_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(new_url)
+    new_html = browser.html 
+    new_soup = bs(new_html, 'lxml')
+    featured_image_url=f"https://www.jpl.nasa.gov{new_soup.find('section', class_='main_feature').a['data-fancybox-href']}"
+    fact_url='https://space-facts.com/mars/'
+    fact_table=pd.read_html(fact_url)
+    df=fact_table[0]
+    hemisphere_image_urls = [
+    {"title": "Valles Marineris Hemisphere", "img_url": "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/valles_marineris_enhanced.tif"},
+    {"title": "Cerberus Hemisphere", "img_url": "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/cerberus_enhanced.tif"},
+    {"title": "Schiaparelli Hemisphere", "img_url": "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/schiaparelli_enhanced.tif"},
+    {"title": "Syrtis Major Hemisphere", "img_url": "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/syrtis_major_enhanced.tif"},
+    ]
+
     data={'news_title': news_title, 'news_p': news_p, 'featured_image_url': featured_image_url,
-    'Mars facts': fact_table, 'Mars Pictues': hemisphere_image_urls}
+    'Mars facts': df, 'Mars Pictues': hemisphere_image_urls}
     browser.quit()
     return data
