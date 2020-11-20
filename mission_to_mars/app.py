56595ddd7c4e62ce7f0e6e9ae9903c6bbf0f353f
Adam Katz <adamkatz@Adams-MacBook-Pro-7.local>
2020-11-20 10:09:20 -0600

    adds proper code to scrape_mars and makes attempts to make proper code for app.py

--- mission_to_mars/app.py
+++ mission_to_mars/app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
+import scrape_mars.py 
 app = Flask(__name__)
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+
+
+@app.route("/")
+def index():
+    data=mongo.db.data.find_one()
+    return render_template('index.html', data=data)
+
+@app.route("/scrape")
+def scrape():
+    mars_data=scrape_mars.scrape()
+    mongo.db.collection.update({}, mars_data, upsert=True)
+    return redirect("/")
+
 
 
 
--- mission_to_mars/scrape_mars.py
+++ mission_to_mars/scrape_mars.py
@@ -1,16 +1,14 @@
 from splinter import Browser
 from bs4 import BeautifulSoup
+import pandas
+import datetime
 
 def init_browser():
     executable_path = {'executable_path': '/Users/adamkatz/.wdm/drivers/chromedriver/mac64/87.0.4280.20/chromedriver'}
     return Browser('chrome', **executable_path, headless=False)
 def scrape():
     browser = init_browser()
-    data={}
-    url = 'https://mars.nasa.gov/news/'
-    browser.visit(url)
-    html = browser.html
-    soup = BeautifulSoup(html, "html.parser")
-    data['news_title']=soup.find_all('div', class_='bottom_gradient')[0].text
-    data['news_p']=soup.find_all('div', class_='article_teaser_body')[0].text
-
+    data={'news_title': news_title, 'news_p': news_p, 'featured_image_url': featured_image_url,
+    'Mars facts': fact_table, 'Mars Pictues': hemisphere_image_urls}
+    browser.quit()
+    return data
